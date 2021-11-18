7ac31195fbe95b6e8a561ee7a929e9b31722741e
Arjun Raj <arjunraj.pala@gmail.com>
2021-11-18 22:58:15 +0530

    updating the blog and jobfinder pages

--- pages/blogs.php
+++ pages/blogs.php
@@ -536,10 +536,11 @@ The one place for all .
       <!-- End of First component layout -->
        <!-- Start of Blog  component layout -->
     <div class="container-fluid py-4">
-      <!-- <div class="row"> -->
-       <div class="col-lg-5">
-          <div class="card h-100 p-3">
-            <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('../assets/img/ivancik.jpg');">
+
+         <div class="col-lg-5">
+           <div class="row">
+            <div class="card h-100 p-3">
+              <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('../assets/img/ivancik.jpg');">
               <span class="mask bg-gradient-dark"></span>
               <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
                 <h5 class="text-white font-weight-bolder mb-4 pt-2">Join the clubs</h5>
@@ -554,18 +555,18 @@ The one place for all .
           <div class="card h-100 p-3">
             <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('../assets/img/ivancik.jpg');">
               <span class="mask bg-gradient-dark"></span>
-              <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
+               <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
                 <h5 class="text-white font-weight-bolder mb-4 pt-2">Join the clubs</h5>
                 <p class="text-white">Wealth creation is an evolutionarily recent positive-sum game. Join clubs in various communities and showcase your talents.</p>
                 <a class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
                   Read More
                   <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
                 </a>
-              </div>
-            </div>
-          </div>
+               </div>
+             </div>
+           </div>
           </div>  
-          </div>
+        </div>
 
       
         
