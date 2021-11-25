9fac4d9d3b424ccbbe810a377455af509f8918d3
Arjun Raj <arjunraj.pala@gmail.com>
2021-11-25 09:47:44 +0530

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -77,7 +77,7 @@ The one place for all .
         </li>
                 <!-- Dashboard Quick Link bar  : Friends tab-->
         <li class="nav-item">
-          <a class="nav-link  " href="../pages/tables.html">
+          <a class="nav-link  " href="../pages/myfriends.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>office</title>
