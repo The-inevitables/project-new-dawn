82bc3e86586b784a057e0cbc9290ca3ac59de903
Arjun Raj <arjunraj.pala@gmail.com>
2021-11-26 23:58:53 +0530

    added images to index

--- index.php
+++ index.php
@@ -76,14 +76,14 @@
       <div class="container" data-aos="fade-up">
         <div class="row justify-content-center">
 
-          <div class="col-md-5 col-lg-3">
-            <img src="assets/img/about-img.jpg" alt="About" data-aos="zoom-in" data-aos-delay="100">
+        <div class="col-md-5 col-lg-5">
+            <img src="assets/img/about.png" alt="About" data-aos="zoom-in" data-aos-delay="100">
           </div>
 
           <div class="col-md-7 col-lg-5">
             <div class="about-content" data-aos="fade-left" data-aos-delay="100">
 
-              <h2><span>Project NEW-DAWN</span>UI Design Mobile </h2>
+              <h2><span>Project NEW-DAWN</span></h2>
               <p>Pandemic has taught us the value of community, what if we can build a better community with the power of technology!
               </p>
 
