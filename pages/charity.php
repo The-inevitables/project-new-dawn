699796696db8fe6adde76fae206ed1b7fe0ef518
Arjun Raj <arjunraj.pala@gmail.com>
2021-11-27 22:48:28 +0530

    bug fixes

--- pages/charity.php
+++ pages/charity.php
@@ -2,7 +2,9 @@
 =========================================================
 Project New Dawn Dashboard
 =========================================================
-The one place for all .
+A community management solution shaped itself in a web- app .
+ Pandemic has taught us the value of community, what if we can build a better community with the power of technology? P
+ roject New Dawn aims at increasing the quality of life in a community with the help of technology.It gives emphasis on providing a better community experience through a webapp.
 
 
 =========================================================
--- pages/communityindex.php
+++ pages/communityindex.php
@@ -2,7 +2,10 @@
 =========================================================
 Project New Dawn Dashboard
 =========================================================
-The one place for all .
+A community management solution shaped itself in a web- app .
+ Pandemic has taught us the value of community, what if we can build a better community with the power of technology? P
+ roject New Dawn aims at increasing the quality of life in a community with the help of technology.It gives emphasis on providing a better community experience through a webapp.
+
 
 
 =========================================================
@@ -492,6 +495,130 @@ The one place for all .
                   </div>
                 </div>
               </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 mt-4">
+          <div class="card mb-4">
+            <div class="card-header pb-0 p-3">
+              <h6 class="mb-1">MY CLUBS</h6>
+              <p class="text-sm">Favourites!</p>
+            </div>
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="position-relative">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="../assets/img/developer.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                      </a>
+                    </div>
+                    <div class="card-body px-1 pb-0">
+                      <p class="text-gradient text-dark mb-2 text-sm"></p>
+                      <a href="javascript:;">
+                        <h5>
+                          Developers Club
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        Place where like minded developers come together to innovate.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <a href="devclub.php" type="button" class="btn btn-outline-primary btn-sm mb-0">Join Club</a>
+                        <div class="avatar-group mt-2">
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                            <img alt="Image placeholder" src="../assets/img/team-1.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                            <img alt="Image placeholder" src="../assets/img/team-2.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                            <img alt="Image placeholder" src="../assets/img/team-3.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                            <img alt="Image placeholder" src="../assets/img/team-4.jpg">
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="position-relative">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="../assets/img/charity.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+                      </a>
+                    </div>
+                    <div class="card-body px-1 pb-0">
+                      <p class="text-gradient text-dark mb-2 text-sm"></p>
+                      <a href="javascript:;">
+                        <h5>
+                          Charity Club
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        Smallest act of kindness is worth more than grandest intention.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <a href="charity.php" type="button" class="btn btn-outline-primary btn-sm mb-0">Join Club</a>
+                        <div class="avatar-group mt-2">
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                            <img alt="Image placeholder" src="../assets/img/team-3.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                            <img alt="Image placeholder" src="../assets/img/team-4.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                            <img alt="Image placeholder" src="../assets/img/team-1.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                            <img alt="Image placeholder" src="../assets/img/team-2.jpg">
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                  <div class="card card-blog card-plain">
+                    <div class="position-relative">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="../assets/img/readers.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                      </a>
+                    </div>
+                    <div class="card-body px-1 pb-0">
+                      <p class="text-gradient text-dark mb-2 text-sm"></p>
+                      <a href="javascript:;">
+                        <h5>
+                         Readers Club
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                      Until I feared I would lose it, I never loved to read. One does not love breathing.
+                      </p>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <a href="readers.php" type="button" class="btn btn-outline-primary btn-sm mb-0">Join Club </a>
+                        <div class="avatar-group mt-2">
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                            <img alt="Image placeholder" src="../assets/img/team-4.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                            <img alt="Image placeholder" src="../assets/img/team-3.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                            <img alt="Image placeholder" src="../assets/img/team-2.jpg">
+                          </a>
+                          <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                            <img alt="Image placeholder" src="../assets/img/team-1.jpg">
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
