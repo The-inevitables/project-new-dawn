ee97f0e9e61f5d66e623770d806004547470fa9d
Arjun Raj <arjunraj.pala@gmail.com>
2021-11-29 09:08:20 +0530

    adding youtube link

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
         <div class="container-fluid container-full" data-aos="zoom-in">
 
           <div class="row">
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q&feature=emb_title" class="glightbox play-btn"></a>
+            <a href="https://www.youtube.com/watch?v=ylB-90nAxvU" class="glightbox play-btn"></a>
           </div>
 
         </div>
